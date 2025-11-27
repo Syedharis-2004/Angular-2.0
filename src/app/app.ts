5aec631f0f09fc3ce61ba8456182162e6a7b82a6
ShaheerShahzadOfficial <mohammedshaheershahzadrawda5@gmail.com>
2025-11-27 14:45:37 +0500

    done

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,17 +2,25 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 import test from 'node:test';
-import { Testimonial } from './testimonial/testimonial';
 import { Projects } from './projects/projects';
 import { Products } from './products/products';
 import { Container } from './container/container';
 import { Servicesportfolio } from './servicesportfolio/servicesportfolio';
 import { Capabilities } from './capabilities/capabilities';
 import { Chooseus } from './chooseus/chooseus';
+import { Testimonials } from './testimonials/testimonials';
 
 @Component({
   selector: 'app-root',
-  imports: [ RouterOutlet, Navbar,Testimonial,Projects,Products,Container,Servicesportfolio,Capabilities,Chooseus,],
+  imports: [ RouterOutlet,
+     Navbar,
+     Projects,
+     Products,
+     Container,
+     Servicesportfolio,
+     Capabilities,
+     Chooseus,
+     Testimonials,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
