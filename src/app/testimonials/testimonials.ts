0acf04fa56270eaa844fe098e75c118f1159050a
Haris Zaidi <salizaidi459@gmail.com>
2025-12-11 17:35:18 +0500

    done

--- src/app/testimonials/testimonials.ts
+++ src/app/testimonials/testimonials.ts
@@ -14,10 +14,10 @@ export class Testimonials implements AfterViewInit {
   currentIndex = 0;
 
   testimonials = [
-    { text: "Libero suscipit unde. Labore cumque accusamus voluptates corporis dignissimos.", img: "assets/user1.jpg", name: "Jorge Monahan", role: "Customer Representative" },
-    { text: "Labore cumque accusamus voluptates corporis dignissimos.", img: "assets/user2.jpg", name: "Adam Lee", role: "Client Manager" },
-    { text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.", img: "assets/user3.jpg", name: "Sarah Khan", role: "Marketing Head" },
-    { text: "Excellent service & professional communication!", img: "assets/user4.jpg", name: "James Smith", role: "HR Manager" }
+    { text: "Libero suscipit unde. Labore cumque accusamus voluptates corporis dignissimos.", img: "assets/images/client 1.png", name: "Jorge Monahan", role: "Customer Representative" },
+    { text: "Labore cumque accusamus voluptates corporis dignissimos.", img: "assets/images/client 2.png", name: "Adam Lee", role: "Client Manager" },
+    { text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.", img: "assets/images/client 3.png", name: "Sarah Khan", role: "Marketing Head" },
+    { text: "Excellent service & professional communication!", img: "assets/images/client 1.png", name: "James Smith", role: "HR Manager" }
   ];
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
